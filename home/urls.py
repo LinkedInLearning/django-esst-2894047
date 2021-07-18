e640ce9baa8b6f4bbb068f86a68d4a51b7dc1687
Leticia Portella <leportella@gmail.com>
2021-07-18 14:30:17 +0100

    recorded successfully

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('home', views.home)
+    path('home', views.home),
+    path('authorized', views.authorized),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
