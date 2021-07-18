9c491e86914454b677842e7149ad6e5cefdd87ff
Leticia Portella <leportella@gmail.com>
2021-07-18 17:03:05 +0100

    recorded successfully

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(models.Notes, NotesAdmin)
