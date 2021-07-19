fddeb98d6dbeaeeb9f4739a6f0916d4fbef13ff0
Leticia Portella <leportella@gmail.com>
2021-07-19 23:00:18 +0100

    recorded successfully

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html' 
+    extra_context = {'today': datetime.today()}
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
