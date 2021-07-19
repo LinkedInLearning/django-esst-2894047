0cbc90c6f2ccef3021e03037fb7cb813a79cbf04
Leticia Portella <leportella@gmail.com>
2021-07-19 23:00:18 +0100

    recorded successfully

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('home', views.home),
-    path('authorized', views.authorized),
+    path('home', views.HomeView.as_view()),
+    path('authorized', views.AuthorizedView.as_view()),
 ]
