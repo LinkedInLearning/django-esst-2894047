4a047e572cba8658d6b76f06a29b5fb263bff979
Leticia Portella <leportella@gmail.com>
2021-07-19 21:01:11 +0100

    recorded successfully

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Notes
+
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
