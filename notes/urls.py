a32b25ca7e2da6b02e31d4d2ace011e53e34651b
Leticia Portella <leportella@gmail.com>
2021-07-27 21:15:28 +0100

    recorded successfully

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
-    path('notes/<int:pk>', views.NotesDetailView.as_view()),
+    path('notes', views.NotesListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
 ]
