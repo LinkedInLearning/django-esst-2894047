16d68c6448a3737b2f0a8fdb34e744bc875ed435
Leticia Portella <leportella@gmail.com>
2021-07-27 22:08:44 +0100

    recorded successfully

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
+    path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
 ]
