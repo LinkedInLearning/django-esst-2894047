c3a84c2e37573c89ac5ca463aabc93f318be28e7
Leticia Portella <leportella@gmail.com>
2021-07-28 22:24:16 +0100

    recorded successfully

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
+    path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name="notes.update"),
     path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,15 @@
 from typing import List
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .forms import NotesForm
 from .models import Notes
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes
