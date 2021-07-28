64dc132caaf3d515d674519d6506e690905ce6e5
Leticia Portella <leportella@gmail.com>
2021-07-28 22:09:28 +0100

    recorded successfully

--- notes/forms.py
+++ notes/forms.py
@@ -7,6 +7,13 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title', 'text')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control my-5'}),
+            'text': forms.Textarea(attrs={"class": "form-control mb-5"})
+        }
+        labels = {
+            'text': 'Write your thoughts here:'
+        }
 
     def clean_title(self):
         title = self.cleaned_data['title']
