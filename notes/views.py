dfa3ec65a95b4ee7a8b1912d02a14c6c8fc3e5a6
Leticia Portella <leportella@gmail.com>
2021-07-28 22:33:26 +0100

    recorded successfully

--- notes/views.py
+++ notes/views.py
@@ -2,9 +2,16 @@ from typing import List
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
+
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
 
 class NotesUpdateView(UpdateView):
     model = Notes
