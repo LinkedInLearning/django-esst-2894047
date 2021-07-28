8d80f19a18d319d6b8f547548a490d5f39a2c006
Leticia Portella <leportella@gmail.com>
2021-07-28 22:44:33 +0100

    recorded successfully

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveSmallIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     
