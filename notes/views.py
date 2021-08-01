db9050deeadbdf6d31feea74e1faa377f6d7adb8
Leticia Portella <leportella@gmail.com>
2021-08-01 19:23:10 +0100

    recorded successfully

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from typing import List
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
@@ -9,20 +10,29 @@ from .forms import NotesForm
 from .models import Notes
 
 
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = "/admin"
 
-class NotesUpdateView(UpdateView):
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
