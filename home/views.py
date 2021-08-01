66d38a8b18bb0ebe0c26a4d6cb4208a0f64b2907
Leticia Portella <leportella@gmail.com>
2021-08-01 22:21:26 +0100

    cleanup

--- home/views.py
+++ home/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,4 @@
-from typing import List
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
-from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
