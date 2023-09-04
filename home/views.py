e617f3e7c8118d68b970e5964452d3fc80540425
Sourabh Vartak <vartak.sourabh@gmail.com>
2023-09-04 18:56:18 +0400

    fixed login url for not logged in users

--- home/views.py
+++ home/views.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
-@login_required()
+@login_required(login_url='/admin')
 def authorized(request):
     return render(request, 'home/authorized.html', {})
