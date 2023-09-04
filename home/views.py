678f93bf3e03591c1b6683369937b4b250c03996
Sourabh Vartak <vartak.sourabh@gmail.com>
2023-09-04 16:33:50 +0400

    added welcome.html template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(request):
-    return HttpResponse('Hello, world!')
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
