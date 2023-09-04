f92e74be615f12ee543ec63c9411af48d7b8481b
Sourabh Vartak <vartak.sourabh@gmail.com>
2023-09-04 20:32:57 +0400

    added /smart/notes section to show list of notes

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('smart/', include('notes.urls')),
 ]
