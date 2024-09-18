dc781c9cf880bf2f31bfd904e052f40609cd315a
AugustoMelo <mcjarod@hotmail.com>
2024-09-18 05:27:43 +0000

    to look errors

--- smartnotes/deployment.py
+++ smartnotes/deployment.py
@@ -7,7 +7,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] 
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
+#DEBUG = False
 
 # WhiteNoise configuration
 MIDDLEWARE = [
