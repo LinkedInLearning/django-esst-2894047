743d7b511e17f66709c0982d14c32555b14a374c
AugustoMelo <mcjarod@hotmail.com>
2024-09-18 04:47:08 +0000

    static root in settings and checking settings in yml

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
