ebd9b516fbeb27ac6a9cf3d07c8bfcd571036373
Leticia Portella <leportella@mini.local>
2024-10-22 17:59:02 +0100

    challenge solution recorded

--- notes/models.py
+++ notes/models.py
@@ -4,4 +4,5 @@ class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveSmallIntegerField(default=0)
     
