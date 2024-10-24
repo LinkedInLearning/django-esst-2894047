400cdd4fdcde0c5c1dcf87f3325d5c79e0ca6872
Leticia Portella <leportella@mini.local>
2024-10-24 19:50:18 +0100

    challenge 07 solution recorded

--- notes/urls.py
+++ notes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name="notes.update"),
     path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name="notes.delete"),
     path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
+    path('notes/<int:pk>/add_like', views.add_like_view, name="notes.add_like")
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,20 @@
 from typing import List
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
+
+def add_like_view(request, pk):
+    if request.method == 'POST':
+        note = get_object_or_404(Notes, pk=pk)
+        note.likes += 1 
+        note.save()
+        return HttpResponseRedirect(reverse("notes.detail", args=(pk,)))
+    raise Http404
 
 
 class NotesDeleteView(DeleteView):
